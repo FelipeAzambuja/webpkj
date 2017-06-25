dfbe8cea7e477b1844afe69bb61b48ba991f988a
principal <principal@principal-PC>
2017-06-25 13:02:34 -0300

    Another tests

--- outra_pagina.php
+++ outra_pagina.php
@@ -1,12 +1,15 @@
 <?php
 function init() {
-    page("outra_pagina.php:root","#pkjpage")->update(["nome"=>"Felipe Nunes Azambuja"]);
+    page("outra_pagina.php:root","#pkjpage")->update(["nome"=>"Felipe Nunes Azambuja","range"=>  range(1, 100)]);
 }
 ?>
 <div page="outra_page_feliz">
-    Bem vindo a outra pagina
+    {{#each range}}
+    Bem vindo a outra pagina {{this}}<br>
+    {{/each}}
 </div>
 <div page="root">
 O {{nome}} veio de outra pagina entre agora na outra page feliz <br>
-<a href="#" onclick="page.go('outra_pagina.php:outra_page_feliz','#pkjpage')"> Outra Pagina feliz</a>
+{{range}}
+<a href="#" onclick="page.go('outra_pagina.php:outra_page_feliz','#pkjpage',{range:[ {{range}} ]})"> Outra Pagina feliz</a>
 </div>
