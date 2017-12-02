92c777bdb653010a1b080a413658ef800da9b948
Felipe Nunes Azambuja <quero@sair.com>
2017-12-02 01:56:33 +0000

    default data

--- public/ajax/pessoas.php
+++ public/ajax/pessoas.php
@@ -1,7 +1,14 @@
 <?php 
 set_time_limit(60);
 $data = [];
-foreach (db()->query("select id,nome from pessoas limit 10000") as $d) {
+$pessoas = [];
+foreach(range(1,10000) as $i){
+    $pessoa = new stdClass();
+    $pessoa->id = $i;
+    $pessoa->nome = 'Felipe Nunes Azambuja';
+    $pessoas[]= $pessoa;
+}
+foreach ($pessoas as $d) {
     $data["data"][] = [
         $d->id,
         $d->nome
